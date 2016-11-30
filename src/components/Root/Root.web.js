e43670ed3a311bdbc75217f447a9883341eb3cca
Aaron Tribou <aaron.tribou@gmail.com>
2016-11-30 16:29:19 -0600

    Checkpoint

---
+++ src/components/Root/Root.web.js
@@ -0,0 +1,23 @@
+// @flow
+import React from 'react'
+import css from './style.css'
+
+
+const Root = (props: { children: any }): React$Element<any> => {
+
+  return (
+    <div
+      className={`${css.root} pt7 tc`}
+    >
+      This is the offline app...
+      {props.children}
+      <ul>
+        <li>This is a test item</li>
+      </ul>
+    </div>
+  )
+
+}
+
+
+export default Root
